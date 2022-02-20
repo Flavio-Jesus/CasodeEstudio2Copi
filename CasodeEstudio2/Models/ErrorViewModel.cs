99732b1e076359471716f2c008597574c6cb24f9
Flavio-Jesus <67942404+Flavio-Jesus@users.noreply.github.com>
2022-02-20 09:56:39 -0600

    Add project files.

---
+++ CasodeEstudio2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CasodeEstudio2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
